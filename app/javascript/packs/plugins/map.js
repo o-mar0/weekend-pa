4fd119a562b60252cbc20f1a2ef271f0c177c63b
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-27 16:36:04 +1100

    add optimisation line

--- app/javascript/packs/plugins/map.js
+++ app/javascript/packs/plugins/map.js
@@ -3,6 +3,8 @@
   packs/plugins/myPlugin.js
 */
 
+import { featureCollection, feature } from '@turf/turf';
+
 export const initMap = (selector) => {
   const elements = Array.from(document.querySelectorAll(selector));
   return elements.map(el => new MyPlugin(el));
@@ -18,19 +20,7 @@ class MyPlugin {
     this.init();
   }
 
-  initMap() {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoiby1tYXIwIiwiYSI6ImNrM2dzaWUyMTA1N2EzbGw5bmU2aTR0cHoifQ.K1SJ2f_lddAklOarADHODw';
-    this.map = new mapboxgl.Map({
-      container: this.el,
-      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-      center: [userLocation.longitude, userLocation.latitude], // starting position [lng, lat]
-      zoom: 12,
-    });
-  }
-
   async init() {
-    this.initMap();
-
     // Array of Location types
     const categoryNames = ["liquor_store", "supermarket", "hospital"];
 
@@ -129,7 +119,7 @@ class MyPlugin {
     // });
 
     // Mapbox GL display markers
-    this.updateMapWithLatestData(userLocation, placeSearchResults);
+    this.initMap(userLocation, placeSearchResults);
   }
 
   removeAllMarkersFromMap() {
@@ -160,13 +150,111 @@ class MyPlugin {
     });
   }
 
+  initMap(userLocation, placeSearchResults) {
+     mapboxgl.accessToken = 'pk.eyJ1Ijoiby1tYXIwIiwiYSI6ImNrM2dzaWUyMTA1N2EzbGw5bmU2aTR0cHoifQ.K1SJ2f_lddAklOarADHODw';
+    this.map = new mapboxgl.Map({
+      container: this.el,
+      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+      center: [144.99125, -37.82394], // starting position [lng, lat]
+      zoom: 12,
+    });
+
+    this.map.on('load', () => {
+      this.updateMapWithLatestData(userLocation, placeSearchResults);
+    });
+  }
+
   updateMapWithLatestData(userLocation, placeSearchResults) {
+    this.map.addSource('route', {
+      type: 'geojson',
+      data: null,
+    });
+    this.map.addLayer({
+      id: 'routeline-active',
+      type: 'line',
+      source: 'route',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#3887be',
+        'line-width': [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          12, 3,
+          22, 12
+        ]
+      }
+    }, 'waterway-label');
+
+    this.map.addLayer({
+      id: 'routearrows',
+      type: 'symbol',
+      source: 'route',
+      layout: {
+        'symbol-placement': 'line',
+        'text-field': '▶',
+        'text-size': [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          12, 24,
+          22, 60
+        ],
+        'symbol-spacing': [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          12, 30,
+          22, 160
+        ],
+        'text-keep-upright': false
+      },
+      paint: {
+        'text-color': '#3887be',
+        'text-halo-color': 'hsl(55, 11%, 96%)',
+        'text-halo-width': 3
+      }
+    }, 'waterway-label');
+
     this.removeAllMarkersFromMap();
     this.addUserMarkerToMap(userLocation);
+    this.addPlaceMarkersToMap(placeSearchResults);
 
     const bounds = new mapboxgl.LngLatBounds();
     placeSearchResults.forEach(placeSearchResult => bounds.extend([ placeSearchResult.location.longitude, placeSearchResult.location.latitude ]));
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+
+    this.getOptimizedRouteBetweenPlaces(userLocation, placeSearchResults);
+  }
+
+  async getOptimizedRouteBetweenPlaces(userLocation, placeSearchResults) {
+    // Store the location of the truck in a variable called coordinates
+      const coordinates = placeSearchResults.map(placeSearchResult => {
+        return `${placeSearchResult.location.longitude},${placeSearchResult.location.latitude}`;
+      });
+
+      coordinates.unshift(`${userLocation.longitude},${userLocation.latitude}`);
+
+      // Set the profile to `driving`
+      // Coordinates will include the current location of the truck,
+      const optimizeUrl = `https://api.mapbox.com/optimized-trips/v1/mapbox/driving/${coordinates.join(';')}?overview=full&steps=true&geometries=geojson&source=first&access_token=${mapboxgl.accessToken}`;
+      const optimizeResult = await fetch(optimizeUrl).then(response => response.json());
+
+      // Create a GeoJSON feature collection
+      var routeGeoJSON = featureCollection([feature(optimizeResult.trips[0].geometry)]);
+
+      // If there is no route provided, reset
+      if (!optimizeResult.trips[0]) {
+          alert('no optimize result');
+      } else {
+        // Update the `route` source by getting the route source
+        // and setting the data equal to routeGeoJSON
+        this.map.getSource('route')
+          .setData(routeGeoJSON);
+      }
   }
 
 }
