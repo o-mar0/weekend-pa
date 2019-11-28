184ab5ebe608ebcda569717ddaa49b12f110ea3d
Vincent Tso <vincent11102@hotmail.com>
2019-11-28 16:43:10 +1100

    add mission javascript to build dynamic map on mission-builder view page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
 
 import { initMap } from './plugins/map';
-import { initMission } from './plugins/mission';
+import { initMissionBuilder } from './plugins/mission';
 
 // Always add a js- prefixed class to your elements when targeting to create a separation
 // of concerns between classes you use to style and classes you use to do javascript.
 initMap('.js-map');
-initMission('.js-cards')
+initMissionBuilder('.js-mission-builder-form')
 
