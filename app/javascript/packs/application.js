2ba18ac813ba1678ca365d7f2af5d5e42b72d70f
Omar Kassem <omar.reda.kassem@gmail.com>
2019-12-01 14:39:23 +1100

    add address input autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import { initMap } from './plugins/map';
 import { initMissionBuilder } from './plugins/mission-builder';
 import { initMission } from './plugins/mission';
 import { initNavbar } from './plugins/navbar';
+import { initAutocomplete } from './plugins/init_autocomplete';
 
 // Always add a js- prefixed class to your elements when targeting to create a separation
 // of concerns between classes you use to style and classes you use to do javascript.
@@ -12,3 +13,4 @@ initMap('.js-map');
 initMissionBuilder('.js-mission-builder-form');
 initMission('.js-mission');
 initNavbar('.js-navbar');
+initAutocomplete();
